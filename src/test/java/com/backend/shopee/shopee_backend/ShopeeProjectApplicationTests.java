5862feced122d065f3a3f5edcaf1c53029a75a7b
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-05-21 12:32:11 -0400

    arrumando

--- src/test/java/com/backend/shopee/shopee_backend/ShopeeProjectApplicationTests.java
+++ src/test/java/com/backend/shopee/shopee_backend/ShopeeProjectApplicationTests.java
@@ -3,9 +3,9 @@ package com.backend.shopee.shopee_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//public class ShopeeProjectApplicationTests {
-//    @Test
-//    void contextLoads() {
-//    }
-//}
+@SpringBootTest
+public class ShopeeProjectApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
