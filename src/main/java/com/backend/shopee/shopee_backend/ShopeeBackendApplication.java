54d4116c1daeab1bd3020d2d319500f7a2b0a339
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-05-21 20:26:16 -0400

    test

--- src/main/java/com/backend/shopee/shopee_backend/ShopeeBackendApplication.java
+++ src/main/java/com/backend/shopee/shopee_backend/ShopeeBackendApplication.java
@@ -2,7 +2,9 @@ package com.backend.shopee.shopee_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!test")
 @SpringBootApplication
 public class ShopeeBackendApplication {
 
