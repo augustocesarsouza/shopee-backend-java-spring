d88e213bebcd02049e8fc98451527bfa9e0f2cc4
augustocesarsouza <augustocesarsantana90@gmail.com>
2025-05-21 20:21:41 -0400

    alterando test

--- src/test/java/com/backend/shopee/shopee_backend/ShopeeProjectApplicationTests.java
+++ src/test/java/com/backend/shopee/shopee_backend/ShopeeProjectApplicationTests.java
@@ -1,11 +1,14 @@
 package com.backend.shopee.shopee_backend;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShopeeProjectApplicationTests {
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
 }
